d760cf46a07e0e16660e902c69d737cbbc7f69dd
yuant <me@yuant.cc>
2025-11-13 01:21:29 +0800

    Hello World!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use systemstat::{Platform, System};
+fn fetch_cpu() {
+    let sys = System::new();
+    match sys.cpu_load_aggregate() {
+        Ok(cpu) => {
+            std::thread::sleep(std::time::Duration::from_secs(1));
+            match cpu.done() {
+                Ok(cpu) => {
+                    println!("CPU Load: {:?}", cpu);
+                }
+                Err(x) => {
+                    eprintln!("Error: {:?}", x);
+                }
+            }
+        }
+        Err(_) => todo!(),
+    }
+}
 fn main() {
     println!("Hello, world!");
+    fetch_cpu();
 }
